2e5a4265ec2dcaeb0173e5540d81700abd84082d
Aziz Hasanain <sgtaziz013@gmail.com>
2021-08-23 11:30:51 +0300

    Update PDFMerger.php

--- src/Facades/PDFMerger.php
+++ src/Facades/PDFMerger.php
@@ -5,7 +5,7 @@ namespace GrofGraf\LaravelPDFMerger\Facades;
 use \Illuminate\Support\Facades\Facade;
 use GrofGraf\LaravelPDFMerger\PDFMerger;
 
-class PDFMergerFacade extends Facade {
+class PDFMerger extends Facade {
 
   protected static function getFacadeAccessor() { return PDFMerger::class; }
 
